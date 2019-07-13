e5b62501d7030723f9b48039da69c5029fe6c01d
Ajitprasad380appu <ajitprasad01011994@gmail.com>
2019-07-13 19:43:13 +0530

    every thing is ok

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -6,10 +6,13 @@ module.exports.create= async function(req,res){
             content:req.body.content,
             user: req.user._id
         });
+        req.flash('success','Post published !');
         return res.redirect('back');
     }catch(err){
-          console.log('Error',err);
-          return;
+          //console.log('Error',err);
+          req.flash('error',err);
+         // return;
+         return res.redirect('back');
     }
   
 }
@@ -19,14 +22,19 @@ module.exports.destroy= async function(req,res){
         if(post.user==req.user.id)
             {
                 post.remove();
-               await  Comment.deleteMany({post:req.params.id});
+               await Comment.deleteMany({post: req.params.id});
+
+               req.flash('success', 'post and associated comments deleted !');
                res.redirect('back');
             }else{
+                req.flash('error','You can not delete post!');
                 return res.redirect('back');
             }
     }catch(err){
-         console.log('Error',err);
-         return ;
+        //  console.log('Error',err);
+            req.flash('error',err);
+        // return ;
+        return res.redirect('back');
     }
    
 }
